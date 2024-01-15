b1083b73ac2904d6b55d1441d73a8ce84bb902ca
Asher <cardosoasher01@gmail.com>
2024-01-15 10:53:23 +0530

    Hello

---
+++ Sybca139Project/src/Sybca139Project/add2nos.java
@@ -0,0 +1,27 @@
+package Sybca139Project;
+
+import java.util.Scanner;
+
+public class add2nos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x ,y ,sum;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter no. 1");
+		x= sc.nextInt();
+		System.out.println("Enter no. 2");
+		y= sc.nextInt();
+		
+		sum=x+y;
+		System.out.println("Sum is " +sum);
+		
+		
+		
+		
+		
+		
+	}
+
+}
