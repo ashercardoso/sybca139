5f425b4e8f448f59af0703c8758ccc8244ff9a5b
Asher <cardosoasher01@gmail.com>
2024-01-15 11:08:29 +0530

    hii

--- Sybca139Project/src/Sybca139Project/add2nos.java
+++ Sybca139Project/src/Sybca139Project/add2nos.java
@@ -7,7 +7,7 @@ public class add2nos {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		int x ,y ,sum;
+		int x ,y ,sum,sub;
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter no. 1");
 		x= sc.nextInt();
@@ -16,6 +16,9 @@ public class add2nos {
 		
 		sum=x+y;
 		System.out.println("Sum is " +sum);
+		
+		sub=x+y;
+		System.out.println("Sub is " +sub);
 		
 		
 		
